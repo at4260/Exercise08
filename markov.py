19ef5c2a7b86c304a8cf8b9dc734be58e915b892
Hackbright Student <info@hackbrightacademy.com>
2015-01-21 18:22:51 -0800

    make chains complete

--- markov.py
+++ markov.py
@@ -14,22 +14,38 @@ def make_chains(filename):
    
     # HELP! remove commas in the string. do we keep punctuation?
     # create the tuple
-    new_tuples = []
-    count1 = 0
+    chains_dict = {}
+    # count1 = 0
+    for index in range(len(words)-2):
+        key = (words[index], words[index+1])
+        value = words[index+2]
+        if key in chains_dict:
+            chains_dict[key].append(value)
+        else:
+            chains_dict[key] = [value]
+
+    print chains_dict
+
     
-    for word in words:
-        while count1 < len(words) - 1:
-            count2 = count1 + 1
-            string_to_tuple = (words[count1], words[count2])
-            new_tuples.append(string_to_tuple)
-            count1 = count1 + 1    
-            # print string_to_tuple
-    #print new_tuples
-    #turn tuples into keys
-    #assign value to key, then turn k,v pair into dictionary
-    for ind_tuple in new_tuples:
-        new_dict = {ind_tuple:'0'}
-        print new_dict
+    # for word in words:
+    #     while count1 < len(words) - 1:
+    #         count2 = count1 + 1
+    #         string_to_tuple = (words[count1], words[count2])
+    #         new_tuples.append(string_to_tuple)
+    #         count1 = count1 + 1    
+    #         # print string_to_tuple
+    # #print new_tuples
+    # #turn tuples into keys
+    # #assign value to key, then turn k,v pair into dictionary
+    # for ind_tuples in new_tuples:
+    #     new_dicts = {ind_tuples:'0'}
+    # #   print new_dicts
+
+    # super_dict = {}
+    # for d in new_dicts:
+    #     for k,v in d.items():
+    #         super_dict[k].append(v)
+    #         print super_dict
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
